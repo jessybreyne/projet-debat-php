7537b5af2788283faeca730d8c0729c710f0a521
Jessy Breyne <jessybreyne@protonmail.com>
2018-10-05 14:55:37 +0200

    Amelioration de l'accueil et mise en place des fichiers php pour remplacer les html

--- pages/pagedebat.php
+++ pages/pagedebat.php
@@ -18,50 +18,9 @@
 
 <body class="bg-light">
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><img src="../img/Debat-Logo-creme.png" style="width: 80px;"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExample04">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">
-            <img  class="nav-img-active" src="../img/round_home_white_48dp.png">
-            Accueil <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <img class="nav-img" src="../img/round_question_answer_white_48dp.png">
-            Mes débats
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img class="nav-img" src="../img/round_person_white_48dp.png">
-        Profil
-      </a>
-          <div class="dropdown-menu" aria-labelledby="dropdown04">
-            <a class="dropdown-item" href="#">Profile</a>
-            <a class="dropdown-item" href="#">Préférences</a>
-            <a class="dropdown-item" href="#">Se deconnecter</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-md-0">
-        <div class="input-group">
-          <input type="text" class="form-control mr-sm" placeholder="" aria-label="" aria-describedby="basic-addon1">
-          <div class="input-group-append">
-            <button class="btn btn-outline-success my-2 my-sm-0 mr-sm-2" type="button">Chercher</button>
-          </div>
-        </div>
-      </form>
-
-      <a class="btn btn-primary my-2 my-sm-0" href="#" role="button">Nouveau débat</a>
-    </div>
-  </nav>
+  <?php
+  include 'menu.php';
+  ?>
 
   <div class="nav-scroller bg-white shadow-sm">
     <nav class="nav nav-underline">
