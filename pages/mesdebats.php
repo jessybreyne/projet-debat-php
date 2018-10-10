d73b2b9d5942ae2d5f5a31b759416aa1577aa45f
Jessy Breyne <jessybreyne@protonmail.com>
2018-10-10 17:36:39 +0200

    Amelioration des fichiers

--- pages/mesdebats.php
+++ pages/mesdebats.php
@@ -68,7 +68,7 @@
         <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
         <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
           <strong class="d-block text-gray-dark">Titre du débat</strong>
-          Auteur
+          10/10/2018 | Auteur
         </p>
       </div>
       <div class="media text-muted pt-3">
