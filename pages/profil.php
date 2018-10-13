a23b16504a0437b30badd8f4aab826b7fc05be13
root <jessybreyne>
2018-10-13 08:21:41 +0200

    Ajout d'une page profil

---
+++ pages/profil.php
@@ -0,0 +1,59 @@
+<?php
+include 'menu.php';
+
+
+// IMPORTATION DES FONCTIONS DE L'API PHP-BD
+require_once("../bd/API-debat.php");
+
+// Démarrer la connexion
+$database = launchPDO("../bd/data");
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>Debat - Profil</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="../css/categorie.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+
+  <div class="nav-scroller bg-white shadow-sm">
+    <nav class="nav nav-underline">
+      <a class="nav-link" href="#">Règlement</a>
+    </nav>
+  </div>
+
+  <main role="main" class="container">
+
+    <div class="my-3 p-3 bg-white rounded shadow-sm">
+      <h6 class="border-bottom border-gray pb-2 mb-0">Profil de <strong><?php echo "Pseudo"; ?></strong></h6>
+
+     
+    </div>
+
+</main>
+
+    <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="../dist/js/jquery-3.2.1.slim.min.js"></script>
+      <script>window.jQuery || document.write('<script src="../dist/js/jquery-slim.min.js"><\/script>')</script>
+      <script src="../dist/js/popper.min.js"></script>
+      <script src="../dist/js/bootstrap.min.js"></script>
+      <script src="../dist/js/holder.min.js"></script>
+      <script src="../js/categorie.js"></script>
+    </body>
+    </html>
