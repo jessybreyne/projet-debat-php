f588795d0857c525076ecff6a8d9d914279a36ff
root <jessybreyne>
2018-10-14 13:55:40 +0200

    Amelioration des preferences

--- pages/preferences.php
+++ pages/preferences.php
@@ -41,6 +41,7 @@ $database = launchPDO("../bd/data");
 
     <div class="my-3 p-3 bg-white rounded shadow-sm">
       <h6 class="border-bottom border-gray pb-2 mb-0"><strong>Préférences</strong></h6>
+      <h6><strong>Maintenance</strong></h6>
       <form class="form-inline">
   <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Site ouvert aux non-admin</label>
   <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
@@ -48,6 +49,30 @@ $database = launchPDO("../bd/data");
     <option value="2">Non</option>
 </select>
   <button type="submit" class="btn btn-primary my-1">Enregistrer</button>
+</form>
+<h6><strong>Changer de mot de passe</strong></h6>
+<form>
+  <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Ancien mot de passe</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Nouveau mot de passe</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Répéter le nouveau mot de passe</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+    </div>
+  </div>
+
+  <button type="submit" class="btn btn-primary my-1">Valider</button>
+
 </form>
     </div>
 </main>
--- pages/profil.php
+++ pages/profil.php
@@ -41,6 +41,12 @@ $database = launchPDO("../bd/data");
 
     <div class="my-3 p-3 bg-white rounded shadow-sm">
       <h6 class="border-bottom border-gray pb-2 mb-0">Profil de <strong><?php echo "Pseudo"; ?></strong></h6>
+      <div class="form-group row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Pseudo</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
     </div>
 </main>
 
