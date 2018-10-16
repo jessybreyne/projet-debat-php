02227051d2bea248f24d50fee31b156b8fe80034
Jessy Breyne <jessybreyne@protonmail.com>
2018-10-16 18:51:39 +0200

    Suppression des paginations

--- pages/mesdebats.php
+++ pages/mesdebats.php
@@ -103,27 +103,6 @@ include 'menu.php';
     </div>
 
 
-    <nav aria-label="..." class="pagination justify-content-center">
-      <ul class="pagination">
-        <li class="page-item disabled">
-          <a class="page-link" href="#" tabindex="-1" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item active">
-          <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
 
   </main>
   <?php
